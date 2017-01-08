5dd61c2fc0d177181a37daa79620fb6c6f84d939
Tom Prince <tprince14@gmail.com>
2017-01-08 17:08:38 -0500

    set up base styles

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
-  // Place JavaScript code here...
-  $('#summertime').summernote();
+
+    // Grab the height of the menu and nudge it down
+    var menuHeight = $('.main-menu').height();
+    var $content = $('.main-content');
+    $content.css('margin-top', (menuHeight) + 'px');
+
+    // Place JavaScript code here...
+    $('#summer-note').summernote({
+        minHeight: 200, // set minimum height of editor
+        maxHeight: 600
+    });
 });
